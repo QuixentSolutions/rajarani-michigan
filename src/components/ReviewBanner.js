81f5dc6fd6c28a80ea644cfa512f3907aa592f4a
Gururajan Ganesan <grajan@edgewatermarkets.com>
2025-08-02 00:18:48 +0530

    added google image

--- src/components/ReviewBanner.js
+++ src/components/ReviewBanner.js
@@ -1,16 +1,18 @@
-import './ReviewBanner.css';
-import { FaGoogle } from 'react-icons/fa';
+import "./ReviewBanner.css";
 
 function ReviewBanner() {
   return (
     <div className="review-banner">
-      <span className="review-text">Review us on Google</span>
-        <a
+      <a
         href="https://www.google.com/search?q=Raja+Rani+Restaurant&stick=H4sIAAAAAAAA_-NgU1I1qLCwME4yNTZMMTZPsTQ0MjK2MqhISbKwNDI0MzK0MDCySDNOWsQqEpSYlagQlJiXqRCUWlySWFqUmFcCAG2OMVdAAAAA&hl=en-GB&mat=CXkHHWDwBxApElcB8pgkaH1MxZK1YEYmOhr9L--IjKRUUN5SaGQDfjKXvZuHgOkyez4TJObGNWdGEsBGtuymvcWTJ0VYn47nPHxgyISdH8w_QHoLHJgIPbP9UAs8dZA7Ef8&authuser=0"
         target="_blank"
         rel="noopener noreferrer"
       >
-        <FaGoogle className="google-icon" />
+        <img
+          src="/Google-Review-Logo.png"
+          alt="Google Review"
+          style={{ width: "50%" }}
+        />
       </a>
     </div>
   );

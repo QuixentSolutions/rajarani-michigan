c3133d332616a39cb8f0606344aed6fa1807972f
Gururajan Ganesan <grajan@edgewatermarkets.com>
2025-08-03 15:37:46 +0800

    Latest changes

--- src/components/ReviewBanner.js
+++ src/components/ReviewBanner.js
@@ -4,7 +4,7 @@ function ReviewBanner() {
   return (
     <div className="review-banner">
       <a
-        href="https://www.google.com/search?q=Raja+Rani+Restaurant&stick=H4sIAAAAAAAA_-NgU1I1qLCwME4yNTZMMTZPsTQ0MjK2MqhISbKwNDI0MzK0MDCySDNOWsQqEpSYlagQlJiXqRCUWlySWFqUmFcCAG2OMVdAAAAA&hl=en-GB&mat=CXkHHWDwBxApElcB8pgkaH1MxZK1YEYmOhr9L--IjKRUUN5SaGQDfjKXvZuHgOkyez4TJObGNWdGEsBGtuymvcWTJ0VYn47nPHxgyISdH8w_QHoLHJgIPbP9UAs8dZA7Ef8&authuser=0"
+        href="https://g.page/r/CTuPAhhiIYnbEAE/review"
         target="_blank"
         rel="noopener noreferrer"
       >

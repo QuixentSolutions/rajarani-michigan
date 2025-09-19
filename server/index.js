2e334ba05931edfa1add74061dfbd53077638a0d
Gururajan Ganesan <grajan@edgewatermarkets.com>
2025-09-20 00:45:09 +0800

    code committed

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const path = require("path");
 const healthRoutes = require("./routes/health");
 const orderRoutes = require("./routes/orders");
 const menuRoutes = require("./routes/menu");

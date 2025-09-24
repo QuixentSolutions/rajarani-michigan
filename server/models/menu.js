d04e55c8d75851215c09c226ce0484f612b90c7b
Rajii-Quixentsolution <rajiiquixentsolution@gmail.com>
2025-09-24 17:53:47 +0530

    my changes

--- server/models/menu.js
+++ server/models/menu.js
@@ -1,16 +1,25 @@
 const mongoose = require("mongoose");
 
 const menuSchema = new mongoose.Schema({
+  _id: { type: String }, 
   title: { type: String, required: true },
   color: String,
   days: [String],
   items: [
     {
+      _id: {
+        type: String,
+        default: () => new mongoose.Types.ObjectId().toString(), // auto-generate for new items
+      },
       name: String,
       price: Number,
       spicelevel: [String],
       addons: [
         {
+          _id: {
+            type: String,
+            default: () => new mongoose.Types.ObjectId().toString(), // auto-generate for new addons
+          },
           name: String,
           price: Number,
         },
